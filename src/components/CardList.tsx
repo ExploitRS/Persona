cc190bc8b33914a47e5cd0736e5913c4aa821a6c
Pandemonica <nillability@protonmail.com>
2023-02-04 04:18:23 +0900

    Update `CardList.tsx`

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -5,10 +5,12 @@ import Card from './Card'
 const CardList: React.FC<CardListProps> = (props: CardListProps) => {
     const m = props
     return (
-        <div>
-            { m.members.map((mem, i) => (
-                <Card username={mem.username} img_src={mem.img_src} is_public={true} ></Card>
-            ))}
+        <div className='flex flex-col justify-center items-start max-w-2xl mx-auto mb-16'>
+            <div className='w-full h-full'>
+                { m.members.map((mem, i) => (
+                    <Card username={mem.username} img_src={mem.img_src} is_public={true} ></Card>
+                ))}
+            </div>
         </div>
     )
 }
