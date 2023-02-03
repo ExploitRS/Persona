bcbef4957955526c53888aacd7907ad6f0f4e78d
Pandemonica <nillability@protonmail.com>
2023-02-03 23:27:39 +0900

    Update `Card.tsx`

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,21 +1,25 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 
-interface CardProps {
-    username: string
-    img_src: string
-}
-
-const Card: React.FC<CardProps> = (username, img_src) => {
+const Card: React.FC<CardProps> = (props: CardProps) => {
+    const user = props
     return (
-        <div>
-            <Link to={`/${username}`} ></Link>
-                <div>
-                    <img src={img_src} alt="" />
+        <Link to={`/${user.username}`} >
+            <div className='bg-cy-blue-mid w-full max-w-sd p-4 border border-cy-blue-light rounded-lg shadow grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8'>
+                <div className='flex'>
+                    <div className='flex-shrink-0'>
+                        <img className='w-8 h-8 rounded-full' src={user.img_src} alt="" />
+                    </div>
+                    <h4 className='text-cy-blue-light flex-1 min-w-0'>{user.username}</h4>
                 </div>
-                <div>username</div>
-        </div>
+            </div>
+        </Link>
     )
 }
 
-export default Card
+export default Card;
+export interface CardProps {
+    username: string
+    img_src: string
+    is_public: boolean
+}
