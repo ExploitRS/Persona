85b194943b54b83d134d36e8d08c72e0c33be532
Pandemonica <nillability@protonmail.com>
2023-02-04 04:20:37 +0900

    `/nil` alias

---
+++ src/pages/nil.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const NilPage = () => {
+    return (
+        <main>
+            <h1>Luca Todesco</h1>
+        </main>
+    )
+}
+
+export const Head = () => <title>exploit-rs::nil</title>
+
+export default NilPage
