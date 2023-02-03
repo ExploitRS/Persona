9de9e43b6fd11f59f4461422ed482f2bf4973628
Pandemonica <nillability@protonmail.com>
2023-02-04 04:19:43 +0900

    Update `Card.tsx`

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,10 +5,10 @@ const Card: React.FC<CardProps> = (props: CardProps) => {
     const user = props
     return (
         <Link to={`/${user.username}`} >
-            <div className='bg-cy-blue-mid w-full max-w-sd p-4 border border-cy-blue-light rounded-lg shadow grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8'>
+            <div className='bg-cy-blue-mid max-w-sd mb-4 p-4 border border-cy-blue-light rounded-lg shadow grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8 items-center rounded'>
                 <div className='flex'>
                     <div className='flex-shrink-0'>
-                        <img className='w-8 h-8 rounded-full' src={user.img_src} alt="" />
+                        <img className='w-10 h-10 rounded-full' src={user.img_src} alt="" />
                     </div>
                     <h4 className='text-cy-blue-light flex-1 min-w-0'>{user.username}</h4>
                 </div>
