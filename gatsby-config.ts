621f4530c4bcc9213d18f9ec4a7b81c1b1280e06
Pandemonica <nillability@protonmail.com>
2023-02-05 08:44:51 +0900

    resolve avatar path and other

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,10 +10,24 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `avatars`,
+    //     path: `${__dirname}/content/avatars`,
+    //   },
+    // },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `config`,
+    //     path: `${__dirname}/content/config`,
+    //   },
+    // },
     'gatsby-plugin-postcss',
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
-    'gatsby-transfomer-sharp',
+    // 'gatsby-transfomer-sharp',
   ],
 }
 
