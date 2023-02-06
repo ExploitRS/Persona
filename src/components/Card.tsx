c9f4d241bde9d12a75d4276955a65e1b34267cee
Pandemonica <nillability@protonmail.com>
2023-02-07 03:00:37 +0900

    Implemented avatar display

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import { GatsbyImage, GatsbyImageProps, getImage, IGatsbyImageData } from 'gatsby-plugin-image'
 
 const Card: React.FC<CardProps> = (props: CardProps) => {
-    const user = props
+    const prsn = props
+    const img = getImage(prsn.avatar)
+
     return (
-        <Link to={`/${user.username}`} >
-            <div className='bg-cy-blue-mid max-w-sd mb-4 p-4 border border-cy-blue-light rounded-lg shadow grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8 items-center rounded'>
+        <Link to={`/${prsn.username}`} >
+            <div className='bg-cy-blue-mid max-w-sd mb-4 p-4 border border-cy-blue-light rounded-lg shadow grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8 items-center content-cneter'>
                 <div className='flex'>
                     <div className='flex-shrink-0'>
-                        <img className='w-10 h-10 rounded-full' src={user.img_src} alt="" />
+                        <GatsbyImage image={img} alt={prsn.username} class='rounded-full' />
                     </div>
-                    <h4 className='text-cy-blue-light flex-1 min-w-0'>{user.username}</h4>
+                    <h4 className='text-cy-blue-light flex-1 min-w-0 mx-4 flex items-center font-bold'>{prsn.username}</h4>
                 </div>
             </div>
         </Link>
@@ -20,6 +23,6 @@ const Card: React.FC<CardProps> = (props: CardProps) => {
 export default Card;
 export interface CardProps {
     username: string
-    img_src: string
     is_public: boolean
+    avatar: IGatsbyImageData
 }
