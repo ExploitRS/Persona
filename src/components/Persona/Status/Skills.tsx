fe4dfc3312d32c307220ad9b16daff79432a0200
Pandemonica <nillability@protonmail.com>
2023-02-14 12:39:58 +0900

    Implemented React component for Skills

--- src/components/Persona/Status/Skills.tsx
+++ src/components/Persona/Status/Skills.tsx
@@ -1,3 +1,23 @@
+import React from "react"
+
 export interface SkillProps {
-    languages: Array<string>,
+    languages?: Array<string>,
 }
+
+const Skills: React.FC<SkillProps> = (props: SkillProps) => {
+    const sk = props;
+
+    return (
+        <div className="hidden p-4 bg-white rounded-lg md:p-8 " id="skills" role="tabpanel" aria-labelledby="skills-tab">
+            { sk.languages && (
+                <>
+                    { sk.languages.map((lang) => (
+                        <div>{ lang }</div>
+                    ))},
+                </>
+            )}
+        </div>
+    )
+}
+
+export default Skills;
