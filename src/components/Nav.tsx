990a39e246c1b41bdcb154ebf026dc94d7621efa
Pandemonica <nillability@protonmail.com>
2023-02-18 22:41:33 +0900

    blur nav background

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,12 +4,12 @@ import { Link } from 'gatsby';
 const Nav = (props: NavProps) => {
     const title = props.title;
     return (
-        <nav className='bg-cy-blue-dark sticky-nav flex justify-between items-center max-w-4xl w-full p-8 my-0 md:my-8 mx-auto opacity-80 top-0 sticky z-50'>
+        <nav className='bg-transparent backdrop-blur sticky-nav flex justify-between items-center max-w-4xl w-full p-8 my-0 md:my-8 mx-auto opacity-80 top-0 sticky z-50'>
             <div className='container flex flex-wrap items-center justify-between mx-auto'>
                 <Link to="#" className='flex items-center'>
                     {/* <img src="logo.svg" className='h-6 mr-3 sm:h-10' alt="logo of Exploit.RS" /> */}
 
-                    <span className='self-center text-xl text-cy-blue-light font-semibold whitespace-nowrap'>{title}</span>
+                    <span className='self-center text-xl text-wild-kingdom-text-green font-bold whitespace-nowrap'>{title}</span>
                 </Link>
                 <button data-collapse-toggle="navbar-dropdown" type="button" className='inline-flex items-center p-2 ml-3 text-sm text-cy-blue-light rouded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200' aria-controls="navbar-dropdown" aria-expanded="false">
                     <span className='sr-only'>Open main menu</span>
