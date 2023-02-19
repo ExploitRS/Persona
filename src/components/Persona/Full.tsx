d3c46db16d2a5ee4fd285ced74d16f8ec945cdd6
Pandemonica <nillability@protonmail.com>
2023-02-19 18:05:34 +0900

    clean Full.tsx for Persona

--- src/components/Persona/Full.tsx
+++ src/components/Persona/Full.tsx
@@ -1,5 +1,5 @@
 import { getImage, IGatsbyImageData, GatsbyImage } from 'gatsby-plugin-image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StatusProps } from './Status';
 import Status from './Status';
 
@@ -16,43 +16,34 @@ export interface PersonaFullProps {
     status?: StatusProps,
 }
 
-// export interface Fluid {
-//     sizes
-// }
-
-
 const PersonaFull: React.FC<PersonaFullProps> = (props: PersonaFullProps) => {
     const p = props
     console.log(p)
     console.log(p.avatar)
-    const avt = getImage(p.avatar)
-    // const style = {
-    //     content: "strcat",
-    // }
+    const avt = getImage(p.avatar.childImageSharp)
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        const timeout = setInterval(() => {
+            setCounter((current) => (current + 1) % aliases.length);
+        }, 4000);
+        
+        return () => clearInterval(timeout);
+    })
+    const aliases = [ 'Luca Todesco', 'nullstr', 'melon' ];
+    let alias = aliases[counter];
 
     return (
         <>
             <div className='w-full max-w-sm bg-wild-kingdom-front-green rounded-lg shadow m-auto opacity-80 demo-1'>
                 <div className='flex flex-col items-center pb-10 content'>
                     <div className='w-24 h-24 mb-3 rounded-full shadow-lg glitch'>
-                        {/* <GatsbyImage className='glitch__image rounded-full' image={ avt } />
-                        <GatsbyImage className='glitch__image rounded-full' image={ avt } />
-                        <GatsbyImage className='glitch__image rounded-full' image={ avt } />
-                        <GatsbyImage className='glitch__image rounded-full' image={ avt } />
-                        <GatsbyImage className='glitch__image rounded-full' image={ avt } /> */}
-                        <GatsbyImage image={getImage(p.avatar.childImageSharp)} className='glitch_image' />
-
+                        <GatsbyImage image={avt} className='glitch_image' />
                     </div>
                     <div className=' justify-center'>
                         <div className='aliases'>
                             <div className='alias'>
-                                <h1 className='text-cy-blue-light mb-1 text-xl font-bold content__title' text='Luca Todesco' >Luca Todesco</h1>
-                            </div>
-                            <div className='alias'>
-                                <h1 className='text-cy-blue-light mb-1 text-xl font-bold content__title' text='nullstr' >nullstr</h1>
-                            </div>
-                            <div className='alias'>
-                                <h1 className='text-cy-blue-light mb-1 text-xl font-bold content__title' text='melon'>melon</h1>
+                                <h1 className='text-cy-blue-light mb-1 text-xl font-bold content__title'>{alias}</h1>
                             </div>
                         </div>
                     </div>
