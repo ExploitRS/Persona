2b3835d6340228a2f9d1ca82c6af7989681b8b9a
Pandemonica <nillability@protonmail.com>
2023-02-22 01:24:30 +0900

    add padding

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -6,7 +6,7 @@ export const Layout: React.FC<any> = ({ children }) => {
     return (
         <>
             <Nav title='Persona' />
-            <main className="bg-gradation-to-r to-wild-kingdom-bg-green-dark from-wild-kingdom-bg-green-light h-full w-full mx-none px-8">
+            <main className="bg-gradation-to-r to-wild-kingdom-bg-green-dark from-wild-kingdom-bg-green-light flex flex-col justify-center h-full w-full mx-auto p-8 max-w-4xl md-my-8">
                 { children }
                 <Footer year='2023' title='Exploit.RS' url=""/>
             </main>
