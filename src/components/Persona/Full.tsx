f02eba532cea9cc951f0ede71647153524809dbb
Pandemonica <nillability@protonmail.com>
2023-02-21 17:08:12 +0900

    Update Persona component

--- src/components/Persona/Full.tsx
+++ src/components/Persona/Full.tsx
@@ -2,6 +2,8 @@ import { getImage, IGatsbyImageData, GatsbyImage } from 'gatsby-plugin-image';
 import React, { useEffect, useState } from 'react';
 import { StatusProps } from './Status';
 import Status from './Status';
+import { ContactsProps, Contact } from './Status/Contacts';
+import { SkillProps } from './Status/Skills';
 
 export interface PersonaFullProps {
     username: string,
@@ -13,13 +15,17 @@ export interface PersonaFullProps {
             fluid: IGatsbyImageData,
         }
     },
-    status?: StatusProps,
+    // status?: StatusProps,
+    contacts?: Contact[],
+    skills?: SkillProps,
 }
 
 const PersonaFull: React.FC<PersonaFullProps> = (props: PersonaFullProps) => {
     const p = props
     console.log(p)
     console.log(p.avatar)
+    console.log('!!!!!!!!!!!!!!!!!Contacts!!!!!!!!!!!!!!!!!!!')
+    console.log(p.contacts)
     const avt = getImage(p.avatar.childImageSharp)
     const [counter, setCounter] = useState(0);
 
@@ -37,24 +43,26 @@ const PersonaFull: React.FC<PersonaFullProps> = (props: PersonaFullProps) => {
         <>
             <div className='w-full max-w-sm bg-wild-kingdom-front-green rounded-lg shadow m-auto opacity-80 demo-1'>
                 <div className='flex flex-col items-center pb-10 content'>
-                    <div className='w-24 h-24 mb-3 rounded-full shadow-lg glitch'>
-                        <GatsbyImage image={avt} className='glitch_image' />
-                    </div>
-                    <div className=' justify-center'>
-                        <div className='aliases'>
-                            <div className='alias'>
-                                <h1 className='text-cy-blue-light mb-1 text-xl font-bold content__title'>{alias}</h1>
+                        <div className='shadow-lg mb-3 p-1 rounded-full bg-gradient-to-r from-wild-kingdom-gradation-green to-wild-kingdom-gradation-purple'>
+                            <div className='w-28 h-28 rounded-full shadow-lg glitch'>
+                                <GatsbyImage image={avt} className='' />
                             </div>
                         </div>
-                    </div>
+                        <div className=' justify-center'>
+                            <div className='aliases'>
+                                <div className='alias'>
+                                    <h1 className='text-cy-blue-light mb-1 text-xl font-bold content__title'>{alias}</h1>
+                                </div>
+                            </div>
+                        </div>
                     <h3 className='text-cy-blue-light text-lg text-gray-500 pb-4'>{ `@${ p.username }` }</h3>
                     <div>
                         <h3 className='text-cy-blue-light text-lg content__text'>{ p.bio }</h3>
                     </div>
                 </div>
             </div>
-            { p.status && (
-                <Status contacts={ p.status.contacts } skills={ p.status.skills } />
+            { p.contacts && (
+                <Status contacts={ p.contacts } skills={ p.skills } />
             )}
         </>
     )
