977157b5b9de8c5c3a2413728299ebb9aae5357b
Pandemonica <nillability@protonmail.com>
2023-02-21 17:04:11 +0900

    add font.tsx

---
+++ src/utils/font.tsx
@@ -0,0 +1,16 @@
+import {
+    faTelegram,
+    faGithub,
+    faBtc,
+} from '@fortawesome/free-brands-svg-icons';
+import {
+    faMailBulk,
+} from '@fortawesome/free-solid-svg-icons';
+
+export const icons = {
+    telegram: faTelegram,
+    email: faMailBulk,
+    github: faGithub,
+    btc: faBtc,
+    notabug: "fa-solid fa-spider-black-widow"
+}
