ae072a769939b1d312505e9273c15a2dcca03404
Pandemonica <nillability@protonmail.com>
2023-02-21 17:06:41 +0900

    fixed: hiddent Skills component

--- src/components/Persona/Status/Skills.tsx
+++ src/components/Persona/Status/Skills.tsx
@@ -8,7 +8,7 @@ const Skills: React.FC<SkillProps> = (props: SkillProps) => {
     const sk = props;
 
     return (
-        <div className="hidden p-4 bg-white rounded-lg md:p-8 " id="skills" role="tabpanel" aria-labelledby="skills-tab">
+        <div className="p-4 bg-white rounded-lg md:p-8 " id="skills" role="tabpanel" aria-labelledby="skills-tab">
             { sk.languages && (
                 <>
                     { sk.languages.map((lang) => (
