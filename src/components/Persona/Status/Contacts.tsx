0b15d1b063d73acdad9f148f38daf4d17b237e56
Pandemonica <nillability@protonmail.com>
2023-02-21 17:07:31 +0900

    Update Contacts component

--- src/components/Persona/Status/Contacts.tsx
+++ src/components/Persona/Status/Contacts.tsx
@@ -1,13 +1,38 @@
 import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { faTelegram } from '@fortawesome/free-solid-svg-icons';
+import { icons } from '../../../utils/font';
 
-export interface ContactProps {
-    email?: Warp,
-    telegram?: Warp,
-    matrix?: Warp,
-    github?: Warp,
-    keybase?: Warp,
-    twitter?: Warp,
-    instagram?: Warp,
+// export interface ContactsProps {
+//     contacts: Contact[],
+// }
+
+export interface ContactsProps {
+    contacts: Contact[];
+}
+
+export type AccountKind =
+    | 'email'
+    | 'telegram'
+    | 'xmpp'
+    | 'keybase'
+    | 'matrix'
+    | 'github'
+    | 'notabug'
+    | 'gitlab'
+    | 'virustotal'
+    | 'discord'
+    | 'twitter'
+    | 'instagram'
+    | 'youtube';
+
+export interface Contact {
+    kind: AccountKind,
+    label: string,
+    id: string,
+    url?: string,
+    quickly?: string,
 }
 
 export interface Warp {
@@ -15,17 +40,21 @@ export interface Warp {
     url: string,
 }
 
-const Contacts: React.FC<ContactProps> = (props: ContactProps) => {
-    const con = props;
+const Contacts: React.FC<ContactsProps> = (props) => {
+    const { contacts: cons } = props;
+    console.log('typeof')
+    console.log(typeof(cons))
+    console.log(cons)
 
     return(
-        <div className="hidden p-4 bg-white rounded-lg md:p-8 " id="contacts" role="tabpanel" aria-labelledby="contacts-tab">
-            { con.telegram && (
-                <>
-                    <div>{ con.telegram.label }</div>
-                    <div>{ con.telegram.url }</div>
-                </>
-            )}
+        <div className="p-4 bg-wild-kingdom-front-green text-wild-kingdom-text-green rounded-lg md:p-8" id="contacts" role="tabpanel" aria-labelledby="contacts-tab">
+            { cons.map((con) => (
+                <div className="text-wild-kingdom-text-green-light">
+                    <FontAwesomeIcon icon={icons[con.kind]} />
+                    {/* <div className="text-cy-blue-light">{ con.telegram.label }</div>
+                    <div>{ con.telegram.url }</div> */}
+                </div>
+            ))}
         </div>
     )
 }
