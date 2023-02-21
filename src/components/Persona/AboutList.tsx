c63782592003d34d497bae2e905bcb649c67aa45
Pandemonica <nillability@protonmail.com>
2023-02-21 17:09:06 +0900

    Update AboutList component

--- src/components/Persona/AboutList.tsx
+++ src/components/Persona/AboutList.tsx
@@ -1,41 +1,22 @@
 import * as React from 'react'
-import { CardProps } from './About'
-import Card from './About'
-import { graphql, useStaticQuery } from 'gatsby'
-import { GatsbyImage, getImage, IGatsbyImageData } from "gatsby-plugin-image";
+import { PersonaAboutProps } from './About'
+import PersonaAbout from './About'
 
-export interface CardListProps {
-    members: Array<CardProps>
+export interface PersonaAboutListProps {
+    personas: Array<PersonaAboutProps>,
 }
 
-export const AboutList = () => {
-    const data = useStaticQuery(graphql`
-        {
-            allAvatarsYaml {
-                nodes {
-                    username
-                    is_public
-                    avatar {
-                        childImageSharp {
-                            gatsbyImageData(
-                                width: 50
-                                height: 50
-                                placeholder: BLURRED
-                                formats: AUTO
-                            )
-                        }
-                    }
-                }
-            }
-        }
-    `)
+const PersonaAboutList: React.FC<PersonaAboutListProps> = (props) => {
+    const personas = props.personas
     return (
         <div className='flex flex-col justify-center items-start max-w-2xl mx-auto mb-16'>
             <div className='w-full h-full'>
-                { data.allAvatarsYaml.nodes.map((persona: CardProps, i: number) => (
-                    <Card username={persona.username} is_public={persona.is_public} avatar={persona.avatar} ></Card>
+                { personas.map((persona: PersonaAboutProps) => (
+                    <PersonaAbout username={persona.username} is_public={persona.is_public} avatar={persona.avatar} />
                 ))}
             </div>
         </div>
     )
 }
+
+export default PersonaAboutList;
