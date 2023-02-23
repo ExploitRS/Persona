aa880ac872c4238a23055120ea43f6c5c1be9ca5
Pandemonica <nillability@protonmail.com>
2023-02-23 17:47:06 +0900

    specify size of FontAwesomeIcon

--- src/components/Persona/Status/Contacts.tsx
+++ src/components/Persona/Status/Contacts.tsx
@@ -45,7 +45,7 @@ const Contacts: React.FC<ContactsProps> = (props) => {
                 { cons.map((con) => (
                     <a href={con.url} target="_blank">
                         <div className="text-wild-kingdom-text-green-light flex flex-col items-center justify-center">
-                            <FontAwesomeIcon className="text-3xl" icon={icons[con.kind]} />
+                            <FontAwesomeIcon className="text-3xl" icon={icons[con.kind]} size='sm' />
                             <dd className="text-cy-blue-light text-sm leading-5 mt-1">{`${ con.id }`}</dd>
                         </div>
                     </a>
