357f4a21b983171f519624a2c2408c8197b51ccb
Pandemonica <nillability@protonmail.com>
2023-02-23 15:10:00 +0900

    Remove button on nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -11,12 +11,6 @@ const Nav = (props: NavProps) => {
 
                     <span className='self-center text-xl text-wild-kingdom-text-green font-bold whitespace-nowrap'>{title}</span>
                 </Link>
-                <button data-collapse-toggle="navbar-dropdown" type="button" className='inline-flex items-center p-2 ml-3 text-sm text-cy-blue-light rouded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200' aria-controls="navbar-dropdown" aria-expanded="false">
-                    <span className='sr-only'>Open main menu</span>
-                    <svg className='w-6 h-6' aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenadd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
-                    </svg>
-                </button>
             </div>
         </nav>
     )
