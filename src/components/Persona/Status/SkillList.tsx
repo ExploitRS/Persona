edbfc0955fa8e64af6cdb001f1764bab06cea48e
Ryo Yamada <mhyprot2@skiff.com>
2023-05-03 11:00:45 +0900

    remove console.log

--- src/components/Persona/Status/SkillList.tsx
+++ src/components/Persona/Status/SkillList.tsx
@@ -7,9 +7,6 @@ export interface SkillsProps {
 
 const Skills: React.FC<SkillsProps> = (props: SkillsProps) => {
     const sk_list = props.skills;
-    console.log('skill list')
-    console.log('skill list')
-    console.log(sk_list)
     return (
         <div className="p-4 bg-white rounded-lg md:p-8 bg-wild-kingdom-front-green" id="skills" role="tabpanel" aria-labelledby="skills-tab">
             { sk_list.map((sk) => (
