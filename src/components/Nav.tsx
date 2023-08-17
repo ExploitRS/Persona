8141c629a705917208e30c8cab13b02cefd27e25
John Paul <nil@exploit.rs>
2023-08-17 17:00:47 +0900

    Update nav component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,12 +4,12 @@ import { Link } from 'gatsby';
 const Nav = (props: NavProps) => {
     const title = props.title;
     return (
-        <nav className='bg-transparent backdrop-blur sticky-nav flex justify-between items-center max-w-4xl w-full p-8 my-0 md:my-8 mx-auto opacity-80 top-0 sticky z-50'>
-            <div className='container flex flex-wrap items-center justify-between mx-auto'>
+        <nav className='backdrop-blur-ios flex justify-between items-center p-8 mx-0 my-0  top-0 sticky z-50'>
+            <div className='container flex flex-wrap items-center justify-between md:mx-auto lg:mx-auto w-screen'>
                 <Link to="/" className='flex items-center'>
                     {/* <img src="logo.svg" className='h-6 mr-3 sm:h-10' alt="logo of Exploit.RS" /> */}
 
-                    <span className='self-center text-xl text-wild-kingdom-text-green font-bold whitespace-nowrap'>{title}</span>
+                    <span className='self-center text-xl text-white font-bold whitespace-nowrap'>{title}</span>
                 </Link>
             </div>
         </nav>
