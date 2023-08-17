1c39708c57a634d2becf27fb5cb6e11d563cb5c5
John Paul <nil@exploit.rs>
2023-08-17 11:02:53 +0900

    Enhancement full persona

--- src/components/Persona/Full.tsx
+++ src/components/Persona/Full.tsx
@@ -29,27 +29,32 @@ const PersonaFull: React.FC<PersonaFullProps> = (props: PersonaFullProps) => {
 
     return (
         <>
-            <div className='w-full max-w-xl bg-wild-kingdom-front-green rounded-lg shadow m-auto opacity-80 demo-1'>
-                <div className='flex flex-col items-center p-10 content'>
-                    <div className='purple-shadow rounded-full mb-3'>
-                        <div className='p-1 rounded-full bg-gradient-to-r from-wild-kingdom-gradation-green to-wild-kingdom-gradation-purple'>
-                            <div className='w-28 h-28 rounded-full glitch'>
-                                <GatsbyImage image={avt} className='' />
+            <div className='w-full max-w-xl rounded-lg shadow m-auto opacity-80 demo-1'>
+                <div className='flex flex-col py-10 content'>
+                    <div className='flex flex-row space-x-8 mx-3'>
+                        <div className='w-32 h-32 rounded-full glitch'>
+                            <GatsbyImage image={avt} className='rounded-full' />
+                        </div>
+                        <div className='my-auto'>
+                            <div className='flex flex-row space-x-4'>
+                                <h1 className='text-white mb-1 text-3xl content__title'>{ p.alias }</h1>
+                                <h3 className='text-lg text-gray-400 mb-2'>{ `@${ p.username }` }</h3>
+                            </div>
+                            <div>
+                                <h2 className='text-white text-lg content__text'>{ p.bio }</h2>
                             </div>
                         </div>
                     </div>
-                    <div className=' justify-center'>
-                        <div className='text-cy-blue-light mb-1 text-xl font-bold content__title'>{ p.alias }</div>
-                    </div>
-                    <h3 className='text-lg text-wild-kingdom-text-green mb-2'>{ `@${ p.username }` }</h3>
-                    <div>
-                        <h3 className='text-cy-blue-light text-lg content__text'>{ p.bio }</h3>
-                    </div>
-                    <div className='text-wild-kingdom-text-green mt-5'>
+                    <div className='text-white mt-5 w-full px-4 rounded-lg flex flex-col bg-blend-hue backdrop-blur-sm bg-black/75 border border-solid border-glass-card-border '>
                         { p.location && (
-                            <div className='flex'>
-                                <FontAwesomeIcon icon={icons['location']} className='mr-2' style={ bioIconSize } />
-                                <h4 className='text-wild-kingdom-text-green leading-5'>{ p.location }</h4>
+                            <div className='flex flex-row py-4 space-x-2 border-b border-glass-card-border'>
+                                <div className='p-2 backdrop-blur-sm bg-black/30 rounded-full'>
+                                    <FontAwesomeIcon icon={icons['location']} className='' style={ bioIconSize } />
+                                </div>
+                                <div className='flex flex-col'>
+                                    <h4 className='text-gray-400 leading-5'>Location</h4>
+                                    <h4 className='text-white leading-5'>{ p.location }</h4>
+                                </div>
                             </div>
                         )}
                         { p.org && (
@@ -59,9 +64,14 @@ const PersonaFull: React.FC<PersonaFullProps> = (props: PersonaFullProps) => {
                             <h4 className='text-wild-kingdom-text-green'>{ p.company }</h4>
                         )}
                         { p.education && (
-                            <div className='flex'>
-                                <FontAwesomeIcon icon={icons['education']} className='mr-2' style={ bioIconSize } />
-                                <h4 className='text-wild-kingdom-text-green leading-5'>{ p.education }</h4>
+                            <div className='flex flex-row space-x-2 py-4'>
+                                <div className='p-2 backdrop-blur-sm bg-black/30 rounded-full'>
+                                    <FontAwesomeIcon icon={icons['education']} className='' style={ bioIconSize } />
+                                </div>
+                                <div className='flex flex-col'>
+                                    <h4 className='text-gray-400 leading-5'>Education</h4>
+                                    <h4 className='text-white leading-5'>{ p.education }</h4>
+                                </div>
                             </div>
                         )}
                     </div>
