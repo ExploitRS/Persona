0fdce138427460723b592b604607e8c800eb2392
John Paul <nil@exploit.rs>
2023-08-19 15:55:26 +0900

    Update status component

--- src/components/Persona/Status/index.tsx
+++ src/components/Persona/Status/index.tsx
@@ -13,19 +13,6 @@ interface TabProps {
 }
 
 const Status: React.FC<StatusProps> = ({ contacts, skills }) => {
-    const initTabs = (st: StatusProps) => {
-        let tabs: TabProps[] = [];
-        { st.contacts && (
-            tabs.push({ title: 'Contacts' })
-        )}
-        { st.skills && (
-            tabs.push({ title: 'Skills' })
-        )}
-        return tabs
-    }
-    const tabs = initTabs({ contacts, skills })
-    const [active, setActive] = useState<string>(tabs[0].title);
-
     const cons = {
         contacts: contacts,
     }
@@ -36,26 +23,12 @@ const Status: React.FC<StatusProps> = ({ contacts, skills }) => {
 
     return(
         <>
-            <div className="w-full bg-wild-kingdom-front-green rounded-lg shadow mt-10 opacity-80 m-auto max-w-xl">
-                <ul className="flex divide-x text-sm font-medium text-center text-wild-kingdom-text-green rounded-t-lg" id="fullWidthTab" data-tabs-toggle="#fullWidthTabContent" role="tablist">
-                    { tabs.map((tab) => (
-                        <li className="w-full">
-                            <button
-                                type="button"
-                                key={ tab.title }
-                                active={ active === tab.title}
-                                onClick={() => setActive(tab.title)}
-                                className="inline-block p-4 text-wild-kingdom-text-green rounded-tl-lg hover:opacity-30">
-                                    { tab.title }
-                            </button>
-                        </li>
-                    ))}
-                </ul>
-                <div id="defaultTabContent" className="border-t-2 gradient-border px-2">
-                    {  active === 'Contacts' && (
+            <div className="w-full rounded-lg shadow mt-10 opacity-80 m-auto max-w-xl">
+                <div id="defaultTabContent" className="">
+                    {  contacts && (
                         <Contacts { ...cons } />
                     )}
-                    {  active === 'Skills' && (
+                    {  skills && (
                         <Skills { ...sks } />
                     )}
                 </div>
