0566d905eabcccd36b1136e1eba10f2e7a17ffb9
John Paul <nil@exploit.rs>
2023-08-20 01:15:14 +0900

    Update profile top

--- src/components/Persona/Full.tsx
+++ src/components/Persona/Full.tsx
@@ -30,16 +30,16 @@ const PersonaFull: React.FC<PersonaFullProps> = (props: PersonaFullProps) => {
 
     return (
         <>
-            <div className='w-full max-w-xl min-w-0 rounded-lg m-auto demo-1'>
+            <div className='justify-center w-full max-w-xl m-auto min-w-0 rounded-lg demo-1'>
                 <div className='flex flex-col py-10 content'>
-                    <div className='flex flex-row space-x-8 mx-3'>
-                        <div className='w-32 h-32 rounded-full glitch'>
+                    <div className='flex flex-row flex-shrink-0 space-x-8'>
+                        <div className='flex-shrink-0 w-[150px] h-[150px] rounded-full glitch'>
                             <GatsbyImage image={avt} className='rounded-full placeholder:blur' />
                         </div>
                         <div className=''>
-                            <div className='flex flex-row space-x-4'>
+                            <div className='flex flex-row flex-wrap space-x-4 items-center'>
                                 <h1 className='text-white mb-1 text-xl md:text-3xl lg:text-3xl font-semibold content__title'>{ p.alias }</h1>
-                                <h3 className='text-lg text-gray-200'>{ `@${ p.username }` }</h3>
+                                <h3 className='text-sm md:text-lg text-gray-200 font-mono font-semibold'>{ `@${ p.username }` }</h3>
                             </div>
                             <div>
                                 <h2 className='text-white text-lg content__text'>{ p.bio }</h2>
