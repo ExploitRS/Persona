39ab57494569fb4b98dfc1961a74d3f4ed5581c6
John Paul <nil@exploit.rs>
2023-08-20 01:16:02 +0900

    Update padding of main view

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -7,7 +7,7 @@ export const Layout: React.FC<any> = ({ children }) => {
         <div className='m-0 p-0 wallpaper'>
             <div className='m-0 p-0  backdrop-blur-ios bg-black/[.2]'>
                 <Nav title='Persona' />
-                <main className="flex-col justify-center w-full mx-auto p-8 max-w-4xl md-my-8">
+                <main className="flex-col justify-center w-full mx-auto p-6 max-w-4xl md-my-8">
                     <div className='min-h-screen '>
                     {/* <div className='min-h-screen bg-test'> */}
                         { children }
