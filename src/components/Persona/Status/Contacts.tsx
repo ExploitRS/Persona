4f266829eb40510206e850fbac943444f84d7357
John Paul <nil@exploit.rs>
2023-08-21 04:30:01 +0900

    Update icons for contact

--- src/components/Persona/Status/Contacts.tsx
+++ src/components/Persona/Status/Contacts.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { library } from '@fortawesome/fontawesome-svg-core';
 // import { faTelegram } from '@fortawesome/free-solid-svg-icons';
-import { icons, contactIconSize } from '../../../utils/font';
+import { icons, contactIconSize, SimpleIcon } from '../../../utils/font';
 export interface ContactsProps {
     contacts: Contact[];
 }
@@ -34,6 +34,15 @@ export interface Warp {
     label: string,
     url: string,
 }
+
+const Icon = {
+    ["email"]: <SimpleIcon name="protonmail" />,
+    ["xmpp"]: <SimpleIcon name="xmpp" />,
+    ["notabug"]: <SimpleIcon name="git" />,
+    ["telegram"]: <FontAwesomeIcon className="text-3xl" icon={icons['telegram']} style={ contactIconSize } />,
+    ["github"]: <FontAwesomeIcon className="text-3xl" icon={icons['github']} style={ contactIconSize } />,
+}
+
 const Contacts: React.FC<ContactsProps> = (props) => {
     const { contacts: cons } = props;
 
@@ -45,7 +54,8 @@ const Contacts: React.FC<ContactsProps> = (props) => {
                     { cons.map((con) => (
                         <a href={con.url} target="_blank" className="flex-grow-0 flex-shrink-0 basis-1/2 mb-2">
                             <div className="text-white flex flex-row space-x-2 justify-start items-center">
-                                <FontAwesomeIcon className="text-3xl" icon={icons[con.kind]} style={ contactIconSize } />
+                                {/* <FontAwesomeIcon className="text-3xl" icon={icons[con.kind]} style={ contactIconSize } /> */}
+                                { Icon[con.kind] }
                                 <div>
                                     <h3 className="text-white text-md font-bold">{ con.label }</h3>
                                     <h4 className="text-gray-400 text-sm font-semibold font-mono">{`${ con.id }`}</h4>
