d02d6f1f9370e4e43734c678284c20e57e5c5b10
John Paul <nil@exploit.rs>
2023-08-21 04:30:11 +0900

    Update layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -4,14 +4,12 @@ import { Footer } from '../components/Footer';
 
 export const Layout: React.FC<any> = ({ children }) => {
     return (
-        <div className='m-0 p-0 wallpaper'>
+        <div className='m-0 p-0'>
             <div className='m-0 p-0  backdrop-blur-ios bg-black/[.2]'>
                 <Nav title='Persona' />
                 <main className="flex-col justify-center w-full mx-auto p-6 max-w-4xl md-my-8">
-                    <div className='min-h-screen '>
                     {/* <div className='min-h-screen bg-test'> */}
                         { children }
-                    </div>
                     <Footer year='2023' title='Exploit.RS' url=""/>
                 </main>
             </div>
