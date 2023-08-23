4d5a9d9281d2f023ed627fe86d0cb8449e24b609
John Paul <nil@exploit.rs>
2023-08-24 02:12:50 +0900

    Update title in Skill list

--- src/components/Persona/Status/SkillList.tsx
+++ src/components/Persona/Status/SkillList.tsx
@@ -9,7 +9,7 @@ const Skills: React.FC<SkillsProps> = (props: SkillsProps) => {
     const sk_list = props.skills;
     return (
         <div className="my-9">
-            <h1 className="text-white font-bold text-2xl md:text-4xl tracking-tight">Skills</h1>
+            <h1 className="text-white font-bold text-2xl md:text-4xl leading-6 tracking-tight my-3 py-2">Skills</h1>
             <div className="flex flex-col flex-nowrap flex-shrink-0 flex-grow-0 basis-full" id="skills">
                 { sk_list.map((sk) => (
                     <Skill { ...sk } />
